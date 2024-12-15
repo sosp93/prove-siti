95dcf1d4483097447dd13ecd682023dbd0979279
sosp93 <eros1993@hotmail.it>
2024-12-15 16:18:52 +0100

    Aggiunta bozza di funziona bussola
    Aggiunti i file bussola.html  e bussola.js per verificare il funzionamento della funzione watchposition

---
+++ bussola.js
@@ -0,0 +1,45 @@
+const degreesVal = document.querySelector('#degrees');
+const btnGps = document.querySelector('#btngps');
+
+function displayPosition(){
+    
+}
+
+// Localizzami con GPS
+function locationSuccess(position){
+    console.log('LOCALIZZAZIONE RIUSCITA');
+    console.log(position);
+    if (position.coords.heading == null) 
+        degreesVal.innerHTML = 'not available';
+    else
+        degreesVal.innerHTML = position.coords.heading
+}
+
+function locationFail(error){
+    console.error('IMPOSSIBILE LOCALIZZARE');
+    pre.innerText = 'LOCALIZZAZIONE FALLITA\nCIAO';
+    switch(error.code) {
+		case error.PERMISSION_DENIED:
+			console.log("Permesso negato dall'utente");
+			break;
+		case error.POSITION_UNAVAILABLE:
+			console.log("Impossibile determinare la posizione corrente");
+			break;
+		case error.TIMEOUT:
+			console.log("Il rilevamento della posizione impiega troppo tempo");
+			break;
+		case error.UNKNOWN_ERROR:
+			console.log("Si è verificato un errore sconosciuto");
+			break;
+	}
+
+}
+let watchPositionID = null;
+
+btnGps.addEventListener('click', function(){
+    // getCurrentPosition è una funzione basata sulle callback
+    console.log('ciao');
+    if (!navigator.geolocation) alert('gps non supportato');
+    watchPositionID = navigator.geolocation.watchPosition(locationSuccess, locationFail/*, gpsOptions*/);
+    //navigator.geolocation.watchPosition(locationSuccess, locationFail/*, gpsOptions*/);
+})

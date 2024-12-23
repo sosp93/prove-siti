542a7c5116f786977abe643757d8a39df4abfed9
sosp93 <eros1993@hotmail.it>
2024-12-23 18:36:44 +0100

    Versione funzionante della bussola

--- bussola.js
+++ bussola.js
@@ -1,86 +1,90 @@
 const degreesVal = document.querySelector('#degrees');
 const note = document.querySelector('#note');
+
 const btnGps = document.querySelector('#btngps');
 const btnBussola = document.querySelector('#compass-toggle');
 const bussola = document.querySelector('#compass-img');
 
-let rotationInterval;
-let degreesBase = 0;
 
+/* VARIABILI */
+let rotationInterval;  //timer ripetitivo per animazione
+
+// Orientamento dispositivo
+// alpha coincide con l'azimut.
+// Angolo [0° , 360°) rispetto alla direzione nord misurata in senso orario
+let alpha = 0; // azimut
+let alphaCont = 0; //azimut corretto per non avere salti
+
+// VISUALIZZA A SCHERMO L'ORIENTAMENTO DEL DISPOSITIVO
+// Se c'è un parametro è fittizio perchè la bussola non è disponibile
 function displayPosition(degrees){
-    /* correggere ed evitare il salto */
-    let customDegrees = degrees;
-    let currentOrientation = bussola.style.transform;
-    console.log(currentOrientation);
-    if (degrees > 180) {
-        console.log('oltre 180');
+    //let currentOrientation = bussola.style.transform;
+    if (degrees == null) {
+        //bussola.style.transform = `rotate(${alphaCont}deg)`;
+        bussola.style.transform = `rotate(0deg)`;
+        degreesVal.innerHTML = `Bussola non disponibile°`;
+        //degreesVal.innerHTML = `Azimut: ${Math.floor(degrees==360 ? 0 : 360 - degrees)}°`;
     }
     else {
-        console.log('prima 180');
+        bussola.style.transform = `rotate(${alphaCont}deg)`;
+        degreesVal.innerHTML = `Azimut: ${Math.floor(alpha)}°`;
     }
-
-    bussola.style.transform = `rotate(${degrees}deg)`;
-    degreesVal.innerHTML = `Azimut: ${Math.floor(degrees==360 ? 0 : 360 - degrees)}°`;
 }
 
-/* Premendo il pulsante accendo o spengo la bussola */
-btnBussola.addEventListener('click', function(){
-    if (btnBussola.innerHTML == 'SPEGNI BUSSOLA') {
-        console.log('spegni');
-        mylistener();
-        window.removeEventListener("deviceorientationabsolute", mylistener, true);
-        btnBussola.innerHTML = 'ACCENDI BUSSOLA';
-    }
-    else {
-        console.log('accendi');
-        window.addEventListener("deviceorientationabsolute", mylistener, true);
-        btnBussola.innerHTML = 'SPEGNI BUSSOLA';
-    }
-})
-
-
-let eventNumber = 0;
-
-let degOld = 0;
-let degCurrent = 0;
-let degCurrentCont = 0;
 
 /* https://developer.mozilla.org/en-US/docs/Web/API/Device_orientation_events/Orientation_and_motion_data_explained */
 
+// PULSANTE ACCENSIONE-SPEGNIMENTO BUSSOLA
+/* Premendo il pulsante accendo o spengo la bussola
+   tramite aggiunta o rimozione dell'event listener*/
+btnBussola.addEventListener('click', function(){
+    if (btnBussola.innerHTML == 'SPEGNI BUSSOLA') {
+        //console.log('spegni');
+        mylistener();
+        window.removeEventListener("deviceorientationabsolute", mylistener, true);
+        btnBussola.innerHTML = 'ACCENDI BUSSOLA';
+        note.innerHTML = 'BUSSOLA SPENTA';
+    }
+    else {
+        //console.log('accendi');
+        mylistener();
+        window.addEventListener("deviceorientationabsolute", mylistener, true);
+        btnBussola.innerHTML = 'SPEGNI BUSSOLA';
+        note.innerHTML = 'BUSSOLA ACCESA';
+    }
+})
+
+// GESTIONE DELL'EVENTO deviceorientationabsolute
 function mylistener (e) {
-    /* Se ho un timer attivo devo come prima cosa spegnerlo*/
-    clearInterval(rotationInterval);
-    if (e == null) console.log('myListener()');
+    // Se non passo l'argomento
+    if (e == null) {
+        /* Se ho un timer attivo lo spengo*/
+        clearInterval(rotationInterval);
+        // azzero la bussola
+        displayPosition();
+    }
+    // Se non ho le informazioni sull'orientamento
     else if (e.alpha == null) {
-        degreesVal.innerHTML = ++eventNumber + ') alpha not available - ' + (e.absolute ? "assoluta" : "relativa");
+        // animazione della bussola
         let j = 0;
+        if (j==0) console.log(e);
         let increment = 10;
         rotationInterval = setInterval(function() {
             oldDeg = j;
             j += increment;
-            console.log('j) ' + j + ' - oldDeg) ' + oldDeg + ' - diff: '+ degVar(oldDeg, j));
             if (j >= 360) increment = -10;
             if (j <-20) clearInterval(rotationInterval);
             bussola.style.transform = `rotate(${(j)}deg)`;
-            //getNegativeDeg(j);
-        }, 30);
+        }, 25/*ms*/);
     }
     else {
-        let degOld = degCurrent;
-        degCurrent = e.alpha;
-        degContinuous = degCurrentCont + degVar(degOld, degCurrent);
-        displayPosition(degCurrentCont);
-        /*
-        degreesVal.innerHTML = ++eventNumber + ') alpha: ' + Math.floor(e.alpha) + ' - ' + (e.absolute ? "assoluta" : "relativa");
-        bussola.style.transform = `rotate(${e.alpha}deg)`;
-        */
+        let degOld = alpha;
+        alpha = e.alpha;
+        alphaCont = alphaCont + degVar(degOld, alpha);
+        displayPosition(alphaCont);
     }
 }
 
-function getNegativeDeg(deg) {
-    console.log (deg + ' >> ' + (deg === 0 ? 0 : deg - 360));
-    return deg === 0 ? 0 : deg - 360;
-}
 
 // restituisce la differenza in gradi tra due valori di angolo
 // nell'intervallo [-180 , +180]
@@ -94,19 +98,3 @@ function degVar(oldVal, newVal) {
     }
     return diff;
 }
-
-
-let x, y;
-x = 180; y = 45;
-console.log (`x old ${x} | y new ${y} | diff x y ${degVar(x,y)}`);
-y = 190;
-console.log (`x old ${x} | y new ${y} | diff x y ${degVar(x,y)}`);
-y = 359;
-console.log (`x old ${x} | y new ${y} | diff x y ${degVar(x,y)}`);
-y = 360;
-console.log (`x old ${x} | y new ${y} | diff x y ${degVar(x,y)}`);
-y = -180;
-console.log (`x old ${x} | y new ${y} | diff x y ${degVar(x,y)}`);
-y = -150;
-console.log (`x old ${x} | y new ${y} | diff x y ${degVar(x,y)}`);
-

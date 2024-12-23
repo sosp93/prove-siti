e7034a7cac81a9e91f7867337688f95690fed008
sosp93 <eros1993@hotmail.it>
2024-12-23 22:55:02 +0100

    Completamento bussola
    Versione funzionante e senza particolari casini nel codice di bussola.

--- bussola.js
+++ bussola.js
@@ -1,3 +1,5 @@
+const mymain = document.querySelector('main');
+
 const degreesVal = document.querySelector('#degrees');
 const note = document.querySelector('#note');
 
@@ -14,6 +16,7 @@ let rotationInterval;  //timer ripetitivo per animazione
 // Angolo [0° , 360°) rispetto alla direzione nord misurata in senso orario
 let alpha = 0; // azimut
 let alphaCont = 0; //azimut corretto per non avere salti
+let compassActive = false;
 
 // VISUALIZZA A SCHERMO L'ORIENTAMENTO DEL DISPOSITIVO
 // Se c'è un parametro è fittizio perchè la bussola non è disponibile
@@ -22,8 +25,7 @@ function displayPosition(degrees){
     if (degrees == null) {
         //bussola.style.transform = `rotate(${alphaCont}deg)`;
         bussola.style.transform = `rotate(0deg)`;
-        degreesVal.innerHTML = `Bussola non disponibile°`;
-        //degreesVal.innerHTML = `Azimut: ${Math.floor(degrees==360 ? 0 : 360 - degrees)}°`;
+        degreesVal.innerHTML = `Bussola non disponibile`;
     }
     else {
         bussola.style.transform = `rotate(${alphaCont}deg)`;
@@ -44,6 +46,7 @@ btnBussola.addEventListener('click', function(){
         window.removeEventListener("deviceorientationabsolute", mylistener, true);
         btnBussola.innerHTML = 'ACCENDI BUSSOLA';
         note.innerHTML = 'BUSSOLA SPENTA';
+        compassActive = false;
     }
     else {
         //console.log('accendi');
@@ -51,6 +54,7 @@ btnBussola.addEventListener('click', function(){
         window.addEventListener("deviceorientationabsolute", mylistener, true);
         btnBussola.innerHTML = 'SPEGNI BUSSOLA';
         note.innerHTML = 'BUSSOLA ACCESA';
+        compassActive = true;
     }
 })
 
@@ -73,10 +77,14 @@ function mylistener (e) {
             oldDeg = j;
             j += increment;
             if (j >= 360) increment = -10;
-            if (j <-20) clearInterval(rotationInterval);
+            if (j <=-360) {
+                clearInterval(rotationInterval);
+                j = -360;
+            }
             bussola.style.transform = `rotate(${(j)}deg)`;
         }, 25/*ms*/);
     }
+    // Se conosco le info sull'orientamento
     else {
         let degOld = alpha;
         alpha = e.alpha;
@@ -98,3 +106,14 @@ function degVar(oldVal, newVal) {
     }
     return diff;
 }
+
+
+// CLICK BUSSOLA
+bussola.addEventListener('click', function() {
+    const newEl = document.createElement('div');
+    newEl.innerHTML = 'CLICK';
+    mymain.appendChild(newEl);
+    setTimeout(() => {
+        mymain.removeChild(newEl);
+    }, 500);
+})

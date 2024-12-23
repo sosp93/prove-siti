de3c1900842e6a2ddf9044b0c7ffbdab28680bed
sosp93 <eros1993@hotmail.it>
2024-12-24 00:45:45 +0100

    Prima sistemazione homepage
    Sistemato index.html con aggiunta bussola, ripulito, migliorato e completato index.css, aggiunto click su item-compass in js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const itemClock = document.querySelector('#item-clock');
 const itemWeather = document.querySelector('#item-weather');
 const itemSs36 = document.querySelector('#item-ss36');
+const itemCompass = document.querySelector('#item-compass');
 
 itemClock.addEventListener('click', function() {
     location.href = "orologio.html"
@@ -14,4 +15,8 @@ itemWeather.addEventListener('click', function() {
 
 itemSs36.addEventListener('click', function() {
     location.href = itemSs36.attributes.goto.value;
+})
+
+itemCompass.addEventListener('click', function() {
+    location.href = itemCompass.attributes.goto.value;
 })
